cbf917d34e66f0246f040cb4a2f3902551d5b87a
米澤圭吾 <yonezawakeigo@yonezawakeigonoMacBook-puro.local>
2020-12-21 18:16:57 +0900

    bug fixed

--- bdm_move_dcmoter.py
+++ bdm_move_dcmoter.py
@@ -18,9 +18,6 @@ GPIO.setup(MOTOR_PWM0, GPIO.OUT)
 pwm0 = GPIO.PWM(MOTOR_PWM0, 50)  # 周波数50Hz
 pwm0.start(0)
 
-spi = spidev.SpiDev()
-spi.open(0, 0) # 0：SPI0、0：CE0
-spi.max_speed_hz = 1000000 # 1MHz SPIのバージョンアップによりこの指定をしないと動かない
 
 try:
     print('--- start program ---')
@@ -34,6 +31,5 @@ except KeyboardInterrupt:
     pass
 finally:
     pwm0.stop()
-    spi.close()
     GPIO.cleanup()
     print('--- stop program ---')
