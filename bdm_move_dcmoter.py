40a9f47ee3996790981e1cb6987c0332e8bb3b7c
Yuki Sakamoto <yuki1293@g.ecc.u-tokyo.ac.jp>
2020-12-21 18:50:04 +0900

    change val

--- bdm_move_dcmoter.py
+++ bdm_move_dcmoter.py
@@ -22,7 +22,7 @@ pwm0.start(0)
 try:
     print('--- start program ---')
     while True:
-        val = 4000
+        val = 2500
         print('val= ',val)
         duty = (val - 2048) * 50 / 2048
         pwm0.ChangeDutyCycle(duty)
