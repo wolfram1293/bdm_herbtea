41875426823b49f22920e088318348394d1a9aa8
米澤圭吾 <yonezawakeigo@yonezawakeigonoMacBook-puro.local>
2020-12-23 19:24:34 +0900

    add move_~~_20.py & move_~~_21.py

--- raspi_app/bdm_move_dcmoter.js
+++ raspi_app/bdm_move_dcmoter.js
@@ -1,3 +1,4 @@
+/*
 var rpio = require('rpio');
  
 var LED_PIN = 40; // GPIO21
@@ -12,3 +13,31 @@ for (var i = 0; i < 1000; i++ ) {
     rpio.write(LED_PIN, rpio.LOW);
     rpio.msleep(500);
 }
+*/
+
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer();
+server.on("request", getJs);
+server.listen(8080);
+console.log("Server running …");
+
+function getJs(req, res) {
+  var url = req.url;
+  console.log(url);
+  if ("/" == url) {
+    fs.readFile("./js.html", "UTF-8", function (err, data) {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.write(data);
+      res.end();
+    });
+  } else if ("/test.js" == url) {
+    fs.readFile("./test.js", "UTF-8", function (err, data) {
+      res.writeHead(200, {"Content-Type": "text/plain"});
+      res.write(data);
+      res.end();
+    });
+  }
+}
+
