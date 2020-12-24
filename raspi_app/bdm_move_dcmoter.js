a4a755455bc1e5d1c954a5df88f01806e87f390b
米澤圭吾 <yonezawakeigo@yonezawakeigonoMacBook-puro.local>
2020-12-24 19:02:33 +0900

    add bdm_~~_gpio12.py and bdm_~~_gpio16.py

--- raspi_app/bdm_move_dcmoter.js
+++ raspi_app/bdm_move_dcmoter.js
@@ -1,43 +1,27 @@
-/*
-var rpio = require('rpio');
- 
-var LED_PIN = 40; // GPIO21
- 
-rpio.open(LED_PIN_1, rpio.OUTPUT, rpio.LOW);
-rpio.open(LED_PIN_2, rpio.OUTPUT, rpio.LOW);
-rpio.open(LED_PIN_3, rpio.OUTPUT, rpio.LOW);
- 
-for (var i = 0; i < 1000; i++ ) {
-    rpio.write(LED_PIN, rpio.HIGH);
+const express = require('express');
+const app = express();
+
+const rpio = require('rpio');
+const LED_PIN21 = 40; // GPIO21
+rpio.open(LED_PIN_21, rpio.OUTPUT, rpio.LOW);
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('hello.ejs');
+});
+
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
+  for (var i = 0; i < 1000; i++ ) {
+    rpio.write(LED_PIN21, rpio.HIGH);
     rpio.msleep(500);
-    rpio.write(LED_PIN, rpio.LOW);
+    rpio.write(LED_PIN21, rpio.LOW);
     rpio.msleep(500);
-}
-*/
+ }
+});
 
-var http = require("http");
-var fs = require("fs");
 
-var server = http.createServer();
-server.on("request", getJs);
-server.listen(8080);
-console.log("Server running …");
-
-function getJs(req, res) {
-  var url = req.url;
-  console.log(url);
-  if ("/" == url) {
-    fs.readFile("./js.html", "UTF-8", function (err, data) {
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.write(data);
-      res.end();
-    });
-  } else if ("/test.js" == url) {
-    fs.readFile("./test.js", "UTF-8", function (err, data) {
-      res.writeHead(200, {"Content-Type": "text/plain"});
-      res.write(data);
-      res.end();
-    });
-  }
-}
-
+app.listen(3000);
