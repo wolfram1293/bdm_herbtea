6a117152adad10c9316b5529a9d5c824d096d37b
wolfram1293 <yuki1293@g.ecc.u-tokyo.ac.jp>
2020-12-27 19:31:07 +0900

    change loadURL

--- app_dcmoter/app_dcmotor-linux-armv7l/resources/app/main.js
+++ app_dcmoter/app_dcmotor-linux-armv7l/resources/app/main.js
@@ -10,7 +10,7 @@ elapp.on("ready", function () {
   var main = new electron.BrowserWindow({width: 800, height: 600});
   main.on("closed", electron.app.quit);
   main.webContents.openDevTools();
-  main.loadURL("http://127.0.0.1:3000/");
+  main.loadFile('public/index.html')
 });
 
 exapp.get("/index", (req, res)=>{
