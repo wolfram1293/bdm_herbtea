180b981fb9ccdae9751f2006951fb750bc584baf
米澤圭吾 <yonezawakeigo@yonezawakeigonoMacBook-puro.local>
2020-12-29 18:44:20 +0900

    changed index.html in app_2020-1228

---
+++ app_2020-1228/python/bdm_move_dcmoter_gpio20.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 18:02:32 2020
+
+@author: Souichirou Kikuchi
+
+"""
+
+import spidev
+import RPi.GPIO as GPIO
+from time import sleep
+
+CHN = 0 # ADコンバーター接続チャンネル
+MOTOR_PWM0 = 20 # DC Motor PWM0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MOTOR_PWM0, GPIO.OUT)
+pwm0 = GPIO.PWM(MOTOR_PWM0, 50)  # 周波数50Hz
+pwm0.start(0)
+
+
+try:
+    for i in range(100):
+        val = 6000
+        print('val= ',val)
+        duty = (val - 2048) * 50 / 2048
+        pwm0.ChangeDutyCycle(duty)
+        sleep(0.5)
+except KeyboardInterrupt:
+    pass
+finally:
+    pwm0.stop()
+    GPIO.cleanup()
+    print('DC moter moved!')
